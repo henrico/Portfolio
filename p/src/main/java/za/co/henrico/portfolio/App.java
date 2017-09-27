2672aeb1b37b73d2ab05434dc7cd185652def65f
henrico__555 <henrico555@gmail.com>
2017-09-27 15:33:05 +0200

    Deploy

--- p/src/main/java/za/co/henrico/portfolio/App.java
+++ p/src/main/java/za/co/henrico/portfolio/App.java
@@ -1,7 +1,10 @@
 package za.co.henrico.portfolio;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -32,6 +35,13 @@ public class App extends SpringBootServletInitializer{
 
 		};
 	}
+	
+	@Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+		ServletRegistration.Dynamic h2ConsoleServlet = servletContext.addServlet("H2Console", new org.h2.server.web.WebServlet());
+        h2ConsoleServlet.addMapping("/h2console/*");
+        h2ConsoleServlet.setLoadOnStartup(1);
+    }
 	
 	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
